d1acde7ac61a0591f3e9f41ffbbc4a21e723f9ab
Gustavo Andrade <gustavo.andrade@eshows.com.br>
2020-05-11 23:36:23 -0300

    Inserts scanlines CSS code

---
+++ frontend/src/components/Profile/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+const Profile = () => {
+  return (
+    <div className="container">
+      <p>gustavoandrade.design</p>
+    </div>
+  );
+};
+
+export default Profile;
