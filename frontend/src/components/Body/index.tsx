15b506e494800f19d8c9512e8b3e86a50e97fd87
Gustavo Andrade <gustavo.gustaandrade@gmail.com>
2022-07-04 18:59:28 -0300

    chore: clean up project 2022 pt1

--- frontend/src/components/Body/index.tsx
+++ frontend/src/components/Body/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { useTranslation } from "react-i18next";
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-import Pill from "../Pill";
+import Pill from '../Pill';
 
-import { Container, PillsArea, RecentProjects } from "./styles";
+import { Container, PillsArea, RecentProjects } from './styles';
 
-import { LanguageItems, PillIcon } from "../../resources/enums";
+import { LanguageItems, PillIcon } from '../../resources/enums';
 
 const Body: React.FC = () => {
   const [t] = useTranslation();
@@ -15,45 +15,45 @@ const Body: React.FC = () => {
       <RecentProjects>{t(LanguageItems.RecentProjects)}</RecentProjects>
       <PillsArea>
         <Pill
-          title="insomnia-plugin-random-cnpj"
-          url="https://github.com/gustaandrade/insomnia-plugin-random-cnpj"
+          title='insomnia-plugin-random-cnpj'
+          url='https://github.com/gustaandrade/insomnia-plugin-random-cnpj'
           icon={PillIcon.npm}
           description={t(LanguageItems.FirstProject)}
         />
 
-        <Pill
+        {/* <Pill
           title="gamedev4noobs"
           url="https://github.com/gustaandrade/gamedev4noobs"
           icon={PillIcon.github}
           description={t(LanguageItems.SecondProject)}
-        />
+        /> */}
 
         <Pill
-          title="gustaandrade.com"
-          url="https://github.com/gustaandrade/gustaandrade.com"
+          title='gustaandrade.com'
+          url='https://github.com/gustaandrade/gustaandrade.com'
           icon={PillIcon.mobile}
           description={t(LanguageItems.ThirdProject)}
         />
 
         <Pill
-          title="gitstories"
-          url="https://github.com/gustaandrade/gitstories"
+          title='gitstories'
+          url='https://github.com/gustaandrade/gitstories'
           icon={PillIcon.react}
           description={t(LanguageItems.FourthProject)}
         />
 
         <Pill
-          title="tomb-keeper"
-          url="https://github.com/gustaandrade/global-game-jam-2020"
+          title='tomb-keeper'
+          url='https://github.com/gustaandrade/global-game-jam-2020'
           icon={PillIcon.unity}
           description={t(LanguageItems.FifthProject)}
         />
 
         <Pill
-          title="star-picross"
-          url="https://github.com/gustaandrade/"
+          title='star-picross'
+          url='https://github.com/gustaandrade/'
           icon={PillIcon.mobile}
-          description={t(LanguageItems.SixthProject)}
+          description={t(LanguageItems.FifthProject)}
         />
       </PillsArea>
     </Container>
--- frontend/src/i18n/translations.ts
+++ frontend/src/i18n/translations.ts
@@ -1,46 +1,46 @@
 const Translations = {
   br: {
     translation: {
-      Role: "Desenvolvedor Web & Desenvolvedor de Jogos",
-      Curriculum: "Currículo",
-      Specialties: "Especialidades",
-      CurrentPalette: "Paleta atual",
-      ChangePalette: "Mudar paleta",
-      RecentProjects: "Projetos recentes",
+      Role: 'Desenvolvedor Web & Desenvolvedor de Jogos',
+      Curriculum: 'Currículo',
+      Specialties: 'Especialidades',
+      CurrentPalette: 'Paleta atual',
+      ChangePalette: 'Mudar paleta',
+      RecentProjects: 'Projetos recentes',
       FirstProject:
-        "<p>Um plugin para o cliente REST Insomnia que gera para o usuário valores randômicos e válidos de CNPJ, pontuados ou não.</p><p>Esse pacote NPM é público e pode ser instalado usando o comando <code>npm install insomnia-plugin-random-cnpj</code> na pasta de plugins do Insomnia.</p>",
+        '<p>Um plugin para o cliente REST Insomnia que gera para o usuário valores randômicos e válidos de CNPJ, pontuados ou não.</p><p>Esse pacote NPM é público e pode ser instalado usando o comando <code>npm install insomnia-plugin-random-cnpj</code> na pasta de plugins do Insomnia.</p>',
       SecondProject:
-        "<p>O gamedev4noobs é um projeto inspirado nos projetos da comunidade He4rt Developers, onde projetos com a tag <code>4noobs</code> concentram repositórios feitos e mantidos pela comunidade sobre diversos conteúdos da área da programação.</p><p>Este em específico aborda o básico do desenvolvimento de um jogo, do zero até o deploy, usando diversas engines para ensinar e exemplificar os passos de criação de um jogo.</p>",
+        '<p>Este portfólio possui uma versão web e mobile, que se complementam. A versão web foi desenvolvida usando React e TypeScript, e a versão mobile foi desenvolvida usando React Native e Expo, além dos pacotes styled-components e i18next em comum.</p><p>A versão mobile está disponível para download, no momento, na Google Play.</p>',
       ThirdProject:
-        "<p>Este portfólio possui uma versão web e mobile, que se complementam. A versão web foi desenvolvida usando React e TypeScript, e a versão mobile foi desenvolvida usando React Native e Expo, além dos pacotes styled-components e i18next em comum.</p><p>A versão mobile está disponível para download, no momento, na Google Play.</p>",
+        '<p>gitstories é um projeto feito em React, utilizando TypeScript, cujo principal objetivo é demonstrar a busca de dados na API do Github de forma assíncrona.</p><p>Ao buscar um usuário do Github, o sistema lista as principais informações de perfil do usuário, como seus repositórios, detalhes desses projetos e informações de perfil.</p>',
       FourthProject:
-        "<p>gitstories é um projeto feito em React, utilizando TypeScript, cujo principal objetivo é demonstrar a busca de dados na API do Github de forma assíncrona.</p><p>Ao buscar um usuário do Github, o sistema lista as principais informações de perfil do usuário, como seus repositórios, detalhes desses projetos e informações de perfil.</p>",
+        '<p>Você é o guardião do túmulo no cemitério da cidade. Durante a noite, coisas estranhas acontecem e muitos danos são causados ​​às lápides.</p><p>Você deve repará-las antes que os portões se abram na manhã seguinte, quando os visitantes chegam. Por quanto tempo você consegue manter o cemitério em boas condições?</p><p>Jogo feito no Unity, usando C#, para o Global Game Jam 2020.</p>',
       FifthProject:
-        "<p>Você é o guardião do túmulo no cemitério da cidade. Durante a noite, coisas estranhas acontecem e muitos danos são causados ​​às lápides.</p><p>Você deve repará-las antes que os portões se abram na manhã seguinte, quando os visitantes chegam. Por quanto tempo você consegue manter o cemitério em boas condições?</p><p>Jogo feito no Unity, usando C#, para o Global Game Jam 2020.</p>",
-      SixthProject:
-        "<p>Star Picross é um jogo em desenvolvimento na engine Unity, usando C#, para dispositivos Android e iOS, com estimativa de lançamento até o final deste ano.</p><p>Ajude a pequena estrela que caiu do céu a reencontrar o seu caminho de volta pra casa, resolvendo diversos puzzles do tipo picross.</p>"
+        '<p>Star Picross é um jogo em desenvolvimento na engine Unity, usando C#, para dispositivos Android e iOS, com estimativa de lançamento até o final deste ano.</p><p>Ajude a pequena estrela que caiu do céu a reencontrar o seu caminho de volta pra casa, resolvendo diversos puzzles do tipo picross.</p>'
+      // SixthProject:
+      // "<p>O gamedev4noobs é um projeto inspirado nos projetos da comunidade He4rt Developers, onde projetos com a tag <code>4noobs</code> concentram repositórios feitos e mantidos pela comunidade sobre diversos conteúdos da área da programação.</p><p>Este em específico aborda o básico do desenvolvimento de um jogo, do zero até o deploy, usando diversas engines para ensinar e exemplificar os passos de criação de um jogo.</p>",
     }
   },
   en: {
     translation: {
-      Role: "Web Developer & Game Developer",
-      Curriculum: "Resume",
-      Specialties: "Specialties",
-      CurrentPalette: "Current palette",
-      ChangePalette: "Change palette",
-      RecentProjects: "Recent projects",
+      Role: 'Web Developer & Game Developer',
+      Curriculum: 'Resume',
+      Specialties: 'Specialties',
+      CurrentPalette: 'Current palette',
+      ChangePalette: 'Change palette',
+      RecentProjects: 'Recent projects',
       FirstProject:
-        "<p>A plugin for Insomnia client that generates random and valid CNPJ values, punctuated or not.</p><p>This public NPM package can be installed using the command <code>npm install insomnia-plugin-random-cnpj</code> on your Insomnia plugin folder.</p>",
+        '<p>A plugin for Insomnia client that generates random and valid CNPJ values, punctuated or not.</p><p>This public NPM package can be installed using the command <code>npm install insomnia-plugin-random-cnpj</code> on your Insomnia plugin folder.</p>',
       SecondProject:
-        "<p>gamedev4noobs is a project inspired by He4rt Developers community projects, where projects tagged 4noobs concentrate repositories made and maintained by the community on various contents involving the programming area.</p><p>This one specifically addresses the basics of game development, from scratch to deploy, using several engines to teach and exemplify the steps of creating a game.</p>",
+        '<p>This portfolio has a web and mobile version, which complement each other. The web version was developed using React and TypeScript, and the mobile version was developed using React Native and Expo, in addition to the styled-components and i18next packages in common.</p><p>The mobile version is currently available for download only on Google Play.</p>',
       ThirdProject:
-        "<p>This portfolio has a web and mobile version, which complement each other. The web version was developed using React and TypeScript, and the mobile version was developed using React Native and Expo, in addition to the styled-components and i18next packages in common.</p><p>The mobile version is currently available for download only on Google Play.</p>",
+        '<p>gitstories is a project made in React, using TypeScript, whose main objective is to demonstrate the search for data in the Github API asynchronously.</p><p>When searching for a Github user, the system lists the main user profile information, such as their repositories, details of these projects and profile information.</p>',
       FourthProject:
-        "<p>gitstories is a project made in React, using TypeScript, whose main objective is to demonstrate the search for data in the Github API asynchronously.</p><p>When searching for a Github user, the system lists the main user profile information, such as their repositories, details of these projects and profile information.</p>",
-      FifthProject:
         "<p>You're the tomb keeper in the city's graveyard. During night, weird things happen and a lot of damage is done to the tombstones.</p><p>You must repair them before the gates open in the next morning, when the visitors arrive. For how long can you keep the graveyard in good conditions?</p><p>Game made in Unit, using C#, for Global Game Jam 2020.</p>",
-      SixthProject:
-        "<p>Star Picross is a game under development on the Unity engine, using C#, for Android and iOS devices, expected to be released until the end of this year.</p><p>Help the little star that fell from the sky to find his way back home, solving various picross-type puzzles.</p>"
+      FifthProject:
+        '<p>Star Picross is a game under development on the Unity engine, using C#, for Android and iOS devices, expected to be released until the end of this year.</p><p>Help the little star that fell from the sky to find his way back home, solving various picross-type puzzles.</p>'
+      // SixthProject:
+      // '<p>gamedev4noobs is a project inspired by He4rt Developers community projects, where projects tagged 4noobs concentrate repositories made and maintained by the community on various contents involving the programming area.</p><p>This one specifically addresses the basics of game development, from scratch to deploy, using several engines to teach and exemplify the steps of creating a game.</p>'
     }
   }
 };
